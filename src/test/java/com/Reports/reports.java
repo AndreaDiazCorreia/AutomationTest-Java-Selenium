4f1c7d710a8665f9ed6d6ff97ef2c9104ef01deb
AndreaDiazCorreia <85651492+AndreaDiazCorreia@users.noreply.github.com>
2023-06-12 19:41:56 -0300

    corrections

--- src/test/java/com/Reports/reports.java
+++ src/test/java/com/Reports/reports.java
@@ -12,7 +12,7 @@ public class reports {
         Date fecha = new Date(Calendar.getInstance().getTimeInMillis());
         SimpleDateFormat formatter = new SimpleDateFormat("ddMMyyyyHHmm");
         String fechaTexto = formatter.format(fecha);
-        String Path = ("src/test/resources/reportes/reports"+fechaTexto+".html");
+        String Path = ("src/test/resources/reports/reports"+fechaTexto+".html");
         ExtentReports extent = new ExtentReports(Path,false);
         return extent;
 
